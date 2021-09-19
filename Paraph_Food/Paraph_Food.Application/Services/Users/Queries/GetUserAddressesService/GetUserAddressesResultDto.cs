15428601630f0030e372aaefbaaaf67d0da0312f
AliEsmaeilzadeh <90599753+AliEsmaeilzadeh@users.noreply.github.com>
2021-09-19 14:49:12 +0430

    Delete Paraph_Food directory

--- Paraph_Food/Paraph_Food.Application/Services/Users/Queries/GetUserAddressesService/GetUserAddressesResultDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Paraph_Food.Application.Services.Users.Queries.GetUserAddressesService
-{
-    public class GetUserAddressesResultDto
-    {
-    }
-}
--- Paraph_Food/Paraph_Food.Domain/Enities/BaseEntities/SimpleEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Paraph_Food.Domain.Enities.BaseEntities
-{
-    public class SimpleEntity
-    {
-        public bool Deleted { get; set; }
-    }
-}
